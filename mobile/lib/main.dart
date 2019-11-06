f9166ce3fd6bdba974c50b98eb65cb2aa6165748
Petr Nymsa <nymsa.petr@outlook.cz>
2019-11-06 10:58:27 +0100

    fix intl locale

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:coffee_time/ui/app.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/intl_standalone.dart';
 
 void main() async {
   await initializeDateFormatting('cs');
   await initializeDateFormatting('en');
+
+  Intl.defaultLocale = await findSystemLocale();
+
   runApp(App());
 }
