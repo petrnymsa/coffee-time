1a88a1344f25f7b1a807c1d0eecf69f484585c04
Petr Nymsa <nymsa.petr@outlook.cz>
2020-02-15 17:47:22 +0100

    #27 Upgrade freezed

--- mobile/lib/core/either.dart
+++ mobile/lib/core/either.dart
@@ -1,8 +1,9 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:meta/meta.dart';
 
 part 'either.freezed.dart';
 
-@immutable
+@freezed
 abstract class Either<L, R> with _$Either<L, R> {
   const factory Either.left(L left) = Left<L, R>;
   const factory Either.right(R right) = Right<L, R>;
--- mobile/lib/domain/failure.dart
+++ mobile/lib/domain/failure.dart
@@ -1,9 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
 
 part 'failure.freezed.dart';
 
-@immutable
+@freezed
 abstract class Failure with _$Failure {
-  const factory Failure(String message) = CommonFailure;
+  const factory Failure(Object inner) = CommonFailure;
   const factory Failure.notFound() = NotFound;
+  const factory Failure.serviceFailure(String message, {Object inner}) =
+      ServiceFailure;
 }
