14c2ff4d3be6db92d9cacdbb7fe5fb180d3c0e6c
Petr Nymsa <nymsa.petr@outlook.cz>
2020-02-15 17:47:53 +0100

    #27 Tag repository

--- mobile/lib/domain/repositories/tags_repository.dart
+++ mobile/lib/domain/repositories/tags_repository.dart
@@ -1,8 +1,10 @@
 import '../../core/either.dart';
 import '../entities/tag.dart';
+import '../entities/tag_reputation.dart';
 import '../failure.dart';
 
 abstract class TagRepository {
   Future<Either<List<Tag>, Failure>> getAll();
-  Future<Either<Tag, NotFound>> getById(String id);
+  Future<Either<Tag, Failure>> getById(String id);
+  Future<Either<List<TagReputation>, Failure>> getForCafe(String placeId);
 }
