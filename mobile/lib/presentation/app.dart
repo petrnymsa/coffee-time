27cb080e1bc5263cb1c0c456c3ece47704e64a56
Petr Nymsa <nymsa.petr@outlook.cz>
2020-02-17 20:44:08 +0100

    Add location geolocator service (#34)
    * #7 Add location geolocator service
    
    * Update mobile.yml

--- mobile/lib/presentation/app.dart
+++ mobile/lib/presentation/app.dart
@@ -1,5 +1,7 @@
+import 'package:coffee_time/domain/entities/location.dart';
 import 'package:coffee_time/presentation/providers/cafe_list.dart';
 import 'package:coffee_time/presentation/screens/home/home.dart';
+import 'package:coffee_time/presentation/services/location_service.dart';
 import 'package:coffee_time/presentation/shared/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -12,12 +14,45 @@ import 'package:provider/provider.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => CafeListProvider()..refresh(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: AppTheme.apply(context),
-        home: HomeScreen(),
+    // return ChangeNotifierProvider(
+    //   create: (_) => CafeListProvider()..refresh(),
+    //   child: MaterialApp(
+    //     title: 'Flutter Demo',
+    //     theme: AppTheme.apply(context),
+    //     home: HomeScreen(),
+    //   ),
+    // );
+    final service = GeolocatorLocationService();
+    final stream = service.listen(distanceFilter: 100);
+
+    return MaterialApp(
+      home: Foo(stream: stream),
+    );
+  }
+}
+
+class Foo extends StatelessWidget {
+  const Foo({
+    Key key,
+    @required this.stream,
+  }) : super(key: key);
+
+  final Stream<Location> stream;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: StreamBuilder(
+          stream: stream,
+          builder: (_, snapshot) {
+            if (snapshot.hasError) return Text(snapshot.error.toString());
+
+            if (snapshot.hasData) return Text(snapshot.data.toString());
+
+            return CircularProgressIndicator();
+          },
+        ),
       ),
     );
   }
