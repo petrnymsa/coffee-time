11898270bb14b07d22a19a5541f431662cdae3fc
Petr Nymsa <nymsa.petr@outlook.cz>
2020-02-18 23:15:17 +0100

    #8 add screens structure

--- mobile/lib/presentation/app.dart
+++ mobile/lib/presentation/app.dart
@@ -1,15 +1,27 @@
-import 'package:coffee_time/domain/entities/location.dart';
-import 'package:coffee_time/presentation/providers/cafe_list.dart';
-import 'package:coffee_time/presentation/screens/home/home.dart';
-import 'package:coffee_time/presentation/services/location_service.dart';
-import 'package:coffee_time/presentation/shared/theme.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
 // just some made up ideas
 //todo layout helper (see ) - see https://www.youtube.com/watch?v=z7P1OFLw4kY
 //todo add logger
 //todo life cycle manager
+
+import 'package:coffee_time/data/repositories/cafe.dart';
+import 'package:coffee_time/data/repositories/tag_repository.dart';
+import 'package:coffee_time/data/services/cafe_service.dart';
+import 'package:coffee_time/data/services/favorite_service.dart';
+import 'package:coffee_time/data/services/photo_service.dart';
+import 'package:coffee_time/data/services/tag_service.dart';
+import 'package:coffee_time/domain/entities/cafe.dart';
+import 'package:coffee_time/domain/entities/location.dart';
+import 'package:coffee_time/domain/services/location_service.dart';
+import 'package:coffee_time/presentation/screens/home/home.dart';
+import 'package:coffee_time/presentation/shared/shared_widgets.dart';
+import 'package:coffee_time/presentation/shared/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart';
+
+import 'core/blocs/cafe_list/bloc.dart';
 
 class App extends StatelessWidget {
   @override
@@ -22,38 +34,90 @@ class App extends StatelessWidget {
     //     home: HomeScreen(),
     //   ),
     // );
-    final service = GeolocatorLocationService();
-    final stream = service.listen(distanceFilter: 100);
-
+    final client = Client();
     return MaterialApp(
-      home: Foo(stream: stream),
+      home: BlocProvider(
+        create: (_) => CafeListBloc(
+            cafeRepository: CafeRepositoryImpl(
+                cafeService: CafeServiceImpl(client: client),
+                favoriteService: FavoriteLocalService(),
+                photoService: PhotoServiceImpl(),
+                tagRepository: TagRepositoryImpl(
+                    tagService: TagServiceImpl(client: client))),
+            locationService:
+                GeolocatorLocationService(geolocator: Geolocator())),
+        child: MaterialApp(
+          title: 'Coffee Time',
+          theme: AppTheme.apply(context),
+          home: HomeScreen(),
+        ),
+      ),
     );
   }
 }
 
 class Foo extends StatelessWidget {
-  const Foo({
+  Foo({
     Key key,
-    @required this.stream,
   }) : super(key: key);
 
-  final Stream<Location> stream;
+  final Client client = Client();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: StreamBuilder(
-          stream: stream,
-          builder: (_, snapshot) {
-            if (snapshot.hasError) return Text(snapshot.error.toString());
+      body: BlocBuilder<CafeListBloc, CafeListState>(
+        builder: (context, state) {
+          return state.when(
+            loading: () => _buildLoading(context),
+            loaded: (cafes) => _buildLoaded(context, cafes),
+            failure: (failure) => _buildFailure(context, failure),
+          );
+        },
+        // builder: (_, snapshot) {
+        //   if (snapshot.hasError) return Text(snapshot.error.toString());
 
-            if (snapshot.hasData) return Text(snapshot.data.toString());
+        //   if (snapshot.hasData) return Text(snapshot.data.toString());
 
-            return CircularProgressIndicator();
-          },
-        ),
+        //   return CircularProgressIndicator();
+        // },
       ),
+    );
+  }
+
+  Widget _buildLoading(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        CircularProgressIndicator(),
+        RaisedButton(
+          child: Text('Load'),
+          onPressed: () => context
+              .bloc<CafeListBloc>()
+              .add(LoadNearby(Location(50.3561972222, 15.9213638889))),
+        )
+      ],
+    );
+  }
+
+  Widget _buildLoaded(BuildContext context, List<Cafe> cafes) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: ListView.builder(
+        itemCount: cafes.length,
+        itemBuilder: (_, i) => Text(cafes[i].name),
+      ),
+    );
+  }
+
+  Widget _buildFailure(BuildContext context, String failure) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(FontAwesomeIcons.exclamationCircle),
+        Text(failure)
+      ],
     );
   }
 }
