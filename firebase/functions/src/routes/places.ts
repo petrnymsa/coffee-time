279bf93e6a51fc8f9f426a3ef742328039d607d1
Petr Nymsa <nymsa.petr@outlook.cz>
2020-02-29 16:09:04 +0100

    #45 pretify

--- firebase/functions/src/routes/places.ts
+++ firebase/functions/src/routes/places.ts
@@ -4,99 +4,106 @@ import { logRequestError } from '../logger';
 import { TagsRepository } from '../firebase/tags';
 
 export const placesRoute = (tagsRepository: TagsRepository): Router => {
+  const router = Router();
+  /**
+   * Required params:
+   * - location
+   * - radius
+   * Optional params:
+   * - opennow
+   * - pagetoken
+   */
+  router.get('/nearby', async (req, res) => {
+    if (!req.query.location) {
+      res.status(400);
+      res.json('location parameter is missing');
+    }
 
-    const router = Router();
-    /**
-     * Required params: 
-     * - location
-     * - radius
-     * Optional params: 
-     * - opennow
-     * - pagetoken
-     */
-    router.get('/nearby', async (req, res) => {
-        if (!req.query.location) {
-            res.status(400);
-            res.json('location parameter is missing');
+    if (!req.query.radius) {
+      res.status(400);
+      res.json('radius parameter is missing');
+    }
+
+    try {
+      const language = req.language;
+      const location = req.query.location;
+      const radius = req.query.radius;
+      const opennow = req.query.opennow;
+      const pagetoken = req.query.pagetoken;
+
+      const nearbyPlaces = await google.getNearby(
+        language,
+        location,
+        radius,
+        opennow,
+        pagetoken
+      );
+
+      if (nearbyPlaces.status === 'OK') {
+        for (const place of nearbyPlaces.results) {
+          const tags = await tagsRepository.getByPlaceId(place.place_id);
+          place.tags = tags;
         }
+      }
 
-        if (!req.query.radius) {
-            res.status(400);
-            res.json('radius parameter is missing');
+      res.json(nearbyPlaces);
+    } catch (err) {
+      logRequestError(req, err);
+      res.status(500).json(err.message);
+    }
+  });
+
+  /**
+   * Required params:
+   * - input
+   * Optional params:
+   * - location and radius
+   */
+  router.get('/find', async (req, res) => {
+    if (!req.query.input) {
+      res.status(400).json('input parameter is missing');
+    }
+
+    try {
+      const input = req.query.input;
+      const language = req.language;
+      const location = req.query.location;
+      const radius = req.query.radius;
+
+      const foundPlaces = await google.findPlaces(
+        input,
+        language,
+        location,
+        radius
+      );
+
+      if (foundPlaces.status === 'OK') {
+        for (const place of foundPlaces.candidates) {
+          const tags = await tagsRepository.getByPlaceId(place.place_id);
+          place.tags = tags;
         }
+      }
 
-        try {
-            const language = req.language;
-            const location = req.query.location;
-            const radius = req.query.radius;
-            const opennow = req.query.opennow;
-            const pagetoken = req.query.pagetoken;
+      res.json(foundPlaces);
+    } catch (err) {
+      logRequestError(req, err);
+      res.status(500).json(err.message);
+    }
+  });
 
-            const nearbyPlaces = await google.getNearby(language, location, radius, opennow, pagetoken);
+  /**
+   * Required params:
+   * - place_id
+   */
+  router.get('/detail/:id', async (req, res) => {
+    try {
+      const detail = await google.getPlaceDetail(req.params.id, req.language);
+      res.json(detail);
+    } catch (err) {
+      logRequestError(req, err);
+      res.status(500).json(err.message);
+    }
+  });
 
-            if (nearbyPlaces.status === 'OK') {
-                for (const place of nearbyPlaces.results) {
-                    const tags = await tagsRepository.getByPlaceId(place.place_id);
-                    place.tags = tags;
-                }
-            }
-
-            res.json(nearbyPlaces);
-        }
-        catch (err) {
-            logRequestError(req, err);
-            res.status(500).json(err.message);
-        }
-    });
-
-    /**
-     * Required params:
-     * - input
-     * Optional params:
-     * - location and radius
-     */
-    router.get('/find', async (req, res) => {
-        if (!req.query.input) {
-            res.status(400).json('input parameter is missing');
-        }
-
-        try {
-            const input = req.query.input;
-            const language = req.language;
-            const location = req.query.location;
-            const radius = req.query.radius;
-
-            const foundPlaces = await google.findPlaces(input, language, location, radius);
-
-            if (foundPlaces.status === 'OK') {
-                for (const place of foundPlaces.candidates) {
-                    const tags = await tagsRepository.getByPlaceId(place.place_id);
-                    place.tags = tags;
-                }
-            }
-
-            res.json(foundPlaces);
-        }
-        catch (err) {
-            logRequestError(req, err);
-            res.status(500).json(err.message);
-        }
-    });
-
-    /**
-     * Required params:
-     * - place_id
-     */
-    router.get('/detail/:id', async (req, res) => {
-        try {
-            const detail = await google.getPlaceDetail(req.params.id, req.language);
-            res.json(detail);
-        }
-        catch (err) {
-            logRequestError(req, err);
-            res.status(500).json(err.message);
-        }
-    });
-
-    return router;
-}
+  return router;
+};
