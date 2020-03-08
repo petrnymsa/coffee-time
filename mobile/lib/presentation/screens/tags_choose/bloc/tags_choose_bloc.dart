9553f241f6a613c9337c91fada3c1b17bda21145
Petr Nymsa <nymsa.petr@outlook.cz>
2020-03-08 22:29:08 +0100

    #68 simplify choose bloc

--- mobile/lib/presentation/screens/tags_choose/bloc/tags_choose_bloc.dart
+++ mobile/lib/presentation/screens/tags_choose/bloc/tags_choose_bloc.dart
@@ -21,30 +21,13 @@ class TagsChooseBloc extends Bloc<TagsChooseBlocEvent, TagsChooseBlocState> {
   Stream<TagsChooseBlocState> mapEventToState(
       TagsChooseBlocEvent event) async* {
     if (event is ChooseTag) {
-      // final wasPresent = state.chosenTags.remove(event.tag);
-      // //todo sort?
-      // if (wasPresent) {
-      //   final availableTags = [...state.availableTags, event.tag];
-      //   availableTags.sort((a, b) => a.id.compareTo(b.id));
-      //   yield Loaded(
-      //       availableTags: availableTags, chosenTags: state.chosenTags);
-      // } else {
-      //   state.availableTags.remove(event.tag);
-      //   final chosenTags = [...state.chosenTags, event.tag];
-      //   chosenTags.sort((a, b) => a.id.compareTo(b.id));
-      //   yield Loaded(
-      //       availableTags: state.availableTags, chosenTags: chosenTags);
-      // }
-
       if (state.chosenTags.contains(event.tag)) {
         yield state.copyWith(
           chosenTags:
               state.chosenTags.where((x) => x.id != event.tag.id).toList(),
         );
       } else {
-        final chosen = [...state.chosenTags, event.tag];
-        chosen.sort((a, b) => a.id.compareTo(b.id));
-        yield state.copyWith(chosenTags: chosen);
+        yield state.copyWith(chosenTags: [...state.chosenTags, event.tag]);
       }
     }
   }
