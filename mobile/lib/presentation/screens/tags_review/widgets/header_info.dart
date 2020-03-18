3c87dd5b2b4c24a67015ce90260becd8b8bf73a0
Petr Nymsa <nymsa.petr@outlook.cz>
2020-03-18 23:26:35 +0100

    #93 Show header info even when no reviews available (#98)

--- mobile/lib/presentation/screens/tags_review/widgets/header_info.dart
+++ mobile/lib/presentation/screens/tags_review/widgets/header_info.dart
@@ -16,25 +16,6 @@ class HeaderInfo extends StatelessWidget {
           textAlign: TextAlign.center,
           style: Theme.of(context).textTheme.subhead,
         ),
-        // RichText(
-        //   textAlign: TextAlign.center,
-        //   text: TextSpan(
-        //       text: 'Pomozte nám ',
-        //       style: Theme.of(context).textTheme.subhead,
-        //       children: [
-        //         TextSpan(
-        //             text: 'zlepšit',
-        //             style: TextStyle(fontWeight: FontWeight.bold)),
-        //         TextSpan(
-        //           text: ' jejich přesnost.',
-        //         )
-        //       ]),
-        // ),
-        SizedBox(height: 6),
-        Text(
-          I18n.of(context).reviews_haederSubTitle,
-          style: TextStyle(fontSize: 14, color: Colors.black54),
-        )
       ],
     );
   }
